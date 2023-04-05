26d4a2af65b67754c6f5488c258060be96e2996c
ilyamed <0ilya@bk.ru>
2023-04-05 09:45:29 +0300

    Fix creation 2

--- src/main.js
+++ src/main.js
@@ -36,8 +36,8 @@ globalThis.onSubmit = (token) => {
 	getById('error__reg').innerText = ''
 	let members = [{}, {}, {}, {}],
 		members_count = +getById('team-length').value
-	for (let i = 0; i < members_count; i++) {
-		members[i] = {
+	for (let i = 1; i <= members_count; i++) {
+		members[i - 1] = {
 			name: getById(`memeber-${i}`).value,
 			email: getById(`memeber-${i}-email`).value,
 			tg: getById(`memeber-${i}-telegram`).value,
@@ -45,7 +45,16 @@ globalThis.onSubmit = (token) => {
 			school: getById(`memeber-${i}-institute`).value,
 		}
 	}
-	const res = { data: { title: getById('team-name').value, members_count, members: [members[0]], member2: [members[1]], member3: [members[2]], member4: [members[3]] } }
+	const res = {
+		data: {
+			title: getById('team-name').value,
+			members_count,
+			members: [members[0]],
+			member2: [members[1]],
+			member3: [members[2]],
+			member4: [members[3]],
+		},
+	}
 	console.log(res)
 	fetch('https://hahaton-mirea.ru/api/teams', {
 		//window.location.origin + '/team', {
